16db1340d2679fac75ec012743833930c555921c
Amlan <amlana.dwivedy@someshwara.com>
2023-04-24 14:24:03 +0700

    Ktlint formatter added

--- zenith/src/main/java/com/amlan/zenith/Extensions.kt
+++ zenith/src/main/java/com/amlan/zenith/Extensions.kt
@@ -1,5 +1,13 @@
 package com.amlan.zenith
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.Resources
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+
 /**
  * This function takes string as param and returns int
  *
@@ -39,3 +47,59 @@ fun getDouble(anyValue: Any?, defaultValue: Double = 0.0): Double {
     return anyValue.toString().getNotNullDouble(defaultValue)
 }
 
+fun Any?.getFloat(defaultValue: Float = 0.0f): Float {
+    if (this == null) {
+        return 0f
+    }
+    var floatValue = defaultValue
+    try {
+        floatValue = this.toString().toFloat()
+    } catch (e: NumberFormatException) {
+        e.printStackTrace()
+    }
+    return floatValue
+}
+
+
+/**
+ *  when you want to show a view
+ */
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+/**
+ *  When you want to hide a view
+ */
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun getScreenWidth(): Int {
+    return Resources.getSystem().displayMetrics.widthPixels
+}
+
+fun getScreenHeight(): Int {
+    return Resources.getSystem().displayMetrics.heightPixels
+}
+
+
+fun View.hideKeyboard(context: Context) {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun Activity.hideKeyBoard() {
+    window?.setSoftInputMode(
+        WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+    )
+}
+
+fun Context.showToast(message: String?, length: Int = Toast.LENGTH_SHORT) =
+    message?.let { toastMessage ->
+        if (toastMessage.isNotEmpty() && toastMessage.isNotBlank()) {
+            Toast.makeText(this, toastMessage, length).show()
+        } else {
+            Toast.makeText(this, this.getString(R.string.something_went_wrong), length).show()
+        }
+    }
